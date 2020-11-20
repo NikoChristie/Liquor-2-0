00c9f1444189179d6c0a9257a503cf9c10b3cd8e
NikoChristie <57568230+NikoChristie@users.noreply.github.com>
2020-11-20 16:53:56 -0600

    Update main.py
    Added try catches, added a exit command and made intervals for listening more frequent

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pyaudio
 import speech_recognition as sr
 import pyttsx3
+import os
+
+os.system('cls')
+
+print('Liquor 2.0')
 
 engine = pyttsx3.init()
 
@@ -8,30 +13,37 @@ r = sr.Recognizer()
 mic = sr.Microphone(device_index=0) 
 
 def main():
+    audio = None
     with mic as source:
         print('listening...')
-        audio = r.listen(source)
-    
-    try:
-        print('running')
-        result = (r.recognize_google(audio)).split()
+        try:
+            audio = r.listen(source, 1, 2)
+        except sr.WaitTimeoutError:
+            pass
+        if audio is not None:
+            try:
+                result = (r.recognize_google(audio)).split()
+                print('running')
 
-        for i in result:
-            if len(i) > 4:
-                s = i
-                
-                if i.endswith('s'):
-                    s = str(s[:-1]) 
-                
-                print(s)
-                if s.endswith(('er', 'or', 'ur')):
-                    print("{0} her? I barely even knew her!".format(s[:-2]))
-                    engine.say("{0} her? I barely even knew her!".format(s[:-2]))
-                    engine.runAndWait()
-    except sr.UnknownValueError:
-        print('???')
-    finally:
-        print('done')
+                for i in result:
+                    if len(i) > 4:
+                        s = i
+                        
+                        if(s == 'terminate'):
+                            engine.say("late her? I barely even knew her!")
+                            engine.runAndWait()
+                            exit()
+
+                        if i.endswith('s'):
+                            s = str(s[:-1]) 
+                        
+                        if s.endswith(('er', 'or', 'ur')):
+                            print("{0} her? I barely even knew her!".format(s[:-2]))
+                            engine.say("{0} her? I barely even knew her!".format(s[:-2]))
+                            engine.runAndWait()
+                print('done')
+            except sr.UnknownValueError:
+                print('???')
     main()
 
 main()
